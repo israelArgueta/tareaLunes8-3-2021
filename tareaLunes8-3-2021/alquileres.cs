db96aecac15ca99a47605915bfee7b1414fe1cf9
israel argueta <israelargueta@mesoamericana.edu.gt>
2021-03-06 17:06:59 -0600

    clase alquileres

---
+++ tareaLunes8-3-2021/alquileres.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tareaLunes8_3_2021
+{
+    class alquileres
+    {
+        string fechalqui;
+            string fechadevo; 
+            double kilrec;
+
+        public string Fechalqui { get => fechalqui; set => fechalqui = value; }
+        public string Fechadevo { get => fechadevo; set => fechadevo = value; }
+        public double Kilrec { get => kilrec; set => kilrec = value; }
+    }
+}
