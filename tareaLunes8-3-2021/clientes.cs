09ea886f62480e3abaf1c570ac4772d9ac59d7b4
israel argueta <israelargueta@mesoamericana.edu.gt>
2021-03-06 16:45:50 -0600

    clases cientes y vehiculos

---
+++ tareaLunes8-3-2021/clientes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tareaLunes8_3_2021
+{
+    class clientes
+    {
+        String nit;
+        string nombre;
+        string direccion;
+
+        public string Nit { get => nit; set => nit = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+    }
+}
---
+++ tareaLunes8-3-2021/vehiculos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tareaLunes8_3_2021
+{
+    class vehiculos
+    {
+        string Placa;
+        string marca;
+        string modelo;
+        string color;
+        double preciki;
+
+        public string Placa1 { get => Placa; set => Placa = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public string Color { get => color; set => color = value; }
+        public double Preciki { get => preciki; set => preciki = value; }
+    }
+}
